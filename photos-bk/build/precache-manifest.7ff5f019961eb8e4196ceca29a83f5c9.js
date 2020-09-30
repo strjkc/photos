bf9d0143d267230bb1903cf4f3bfacd2dbb36e6a
strahinja <strahinjaa.va@gmail.com>
2020-09-30 21:54:41 +0200

    build

---
+++ photos-bk/build/precache-manifest.7ff5f019961eb8e4196ceca29a83f5c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6599f9ea50cb228d92de5cc4800474",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba5c6f029512ce879f1e",
+    "url": "/static/css/main.92886e26.chunk.css"
+  },
+  {
+    "revision": "dcf5492f9a091e083d21",
+    "url": "/static/js/2.264420fc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.264420fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5c6f029512ce879f1e",
+    "url": "/static/js/main.c0514a03.chunk.js"
+  },
+  {
+    "revision": "28259804c27cb7befbb8",
+    "url": "/static/js/runtime-main.b7579e6d.js"
+  },
+  {
+    "revision": "29f9416be746bfd6d30cc042c4c6192a",
+    "url": "/static/media/IMG_20200908_123323_193-01.29f9416b.jpeg"
+  },
+  {
+    "revision": "c10403b18088ef839ffed1899ab49e4c",
+    "url": "/static/media/KumbhSans-Regular.c10403b1.ttf"
+  },
+  {
+    "revision": "aaa5880c7a5f7e479e31a4412452d8a9",
+    "url": "/static/media/Satisfy-Regular.aaa5880c.ttf"
+  }
+]);
